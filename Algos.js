86b7a79a382859c5e13ba442422cb09508b927a2
Tyler Thibault <tyler.thibault@protonmail.com>
2023-03-09 08:11:09 -0700

    Updated for Thursday

--- Algos.js
+++ Algos.js
@@ -166,60 +166,92 @@ class ListNode {
      * @returns {any} The data from the node that was removed.
     */
     removeBack() {}
-
     
-    /**
-      * Determines whether or not the given search value exists in this list.
-      * - Time: O(?).
-      * - Space: O(?).
-      * @param {any} val The data to search for in the nodes of this list.
-      * @returns {boolean}
-    */
-    contains(val) {}
     
     /**
      * Determines whether or not the given search value exists in this list.
      * - Time: O(?).
      * - Space: O(?).
      * @param {any} val The data to search for in the nodes of this list.
+     * @returns {boolean}
+    */
+   contains(val) {}
+   
+   /**
+    * Determines whether or not the given search value exists in this list.
+    * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} val The data to search for in the nodes of this list.
      * @param {?ListNode} current The current node during the traversal of this list
      *    or null when the end of the list has been reached.
      * @returns {boolean}
-    */
-    containsRecursive(val, current = this.head) {}
+   */
+  containsRecursive(val, current = this.head) {}
 
-    // EXTRA
-    /**
-     * Recursively finds the maximum integer data of the nodes in this list.
-     * - Time: O(?).
-     * - Space: O(?).
-     * @param {ListNode} runner The start or current node during traversal, or null
-     *    when the end of the list is reached.
-     * @param {ListNode} maxNode Keeps track of the node that contains the current
-     *    max integer as it's data.
-     * @returns {?number} The max int or null if none.
-     */
-    recursiveMax(runner = this.head, maxNode = this.head) {}
-
+  // EXTRA
+  /**
+   * Recursively finds the maximum integer data of the nodes in this list.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {ListNode} runner The start or current node during traversal, or null
+   *    when the end of the list is reached.
+   * @param {ListNode} maxNode Keeps track of the node that contains the current
+   *    max integer as it's data.
+   * @returns {?number} The max int or null if none.
+  */
+ recursiveMax(runner = this.head, maxNode = this.head) {}
+ 
  // ********************* END WEDNESDAY *********************
-  
-    /**
-     * Converts this list into an array containing the data of each node.
-     * - Time: O(n) linear.
-     * - Space: O(n).
-     * @returns {Array<any>} An array of each node's data.
-     */
-    toArr() {
+ // ********************* THURSDAY *********************
+ /**
+ * Retrieves the data of the second to last node in this list.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @returns {any} The data of the second to last node or null if there is no
+ *    second to last node.
+ */
+secondToLast() {}
+
+/**
+ * Removes the node that has the matching given val as it's data.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {any} val The value to compare to the node's data to find the
+ *    node to be removed.
+ * @returns {boolean} Indicates if a node was removed or not.
+ */
+removeVal(val) {}
+
+// EXTRA
+/**
+ * Inserts a new node before a node that has the given value as its data.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {any} newVal The value to use for the new node that is being added.
+ * @param {any} targetVal The value to use to find the node that the newVal
+ *    should be inserted in front of.
+ * @returns {boolean} To indicate whether the node was pre-pended or not.
+ */
+prepend(newVal, targetVal) {}
+
+ // ********************* END THURSDAY *********************
+ /**
+  * Converts this list into an array containing the data of each node.
+  * - Time: O(n) linear.
+  * - Space: O(n).
+  * @returns {Array<any>} An array of each node's data.
+ */
+toArr() {
       const arr = [];
       let runner = this.head;
-  
+      
       while (runner) {
         arr.push(runner.data);
         runner = runner.next;
       }
       return arr;
     }
-
+    
     // Method to print the linked list and its nodes
     printList() {
       let current = this.head;
@@ -232,13 +264,13 @@ class ListNode {
       console.log(list);
       return this
     }
-}
+  }
   
   /******************************************************************* 
-  Multiple test lists already constructed to test your methods on.
-  Below commented code depends on insertAtBack method to be completed,
-  after completing it, uncomment the code.
-  */
+   Multiple test lists already constructed to test your methods on.
+   Below commented code depends on insertAtBack method to be completed,
+   after completing it, uncomment the code.
+   */
   const emptyList = new SinglyLinkedList();
   
   // const singleNodeList = new SinglyLinkedList().insertAtBackMany([1]);
@@ -246,8 +278,8 @@ class ListNode {
   // const firstThreeList = new SinglyLinkedList().insertAtBackMany([1, 2, 3]);
   // const secondThreeList = new SinglyLinkedList().insertAtBackMany([4, 5, 6]);
   const unorderedList = new SinglyLinkedList().insertAtBackMany([
-      -5, -10, 4, -3, 6, 1, -7, -2,
-    ]);
+    -5, -10, 4, -3, 6, 1, -7, -2,
+  ]);
   unorderedList.printList()
   
   /* node 4 connects to node 1, back to head */
