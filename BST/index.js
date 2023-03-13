d23524e88aa8a34a4fdcfc7cfc450b6c0264de3c
Tyler Thibault <tyler.thibault@protonmail.com>
2023-03-13 09:04:03 -0600

    update BST

--- BST/index.js
+++ BST/index.js
@@ -7,45 +7,45 @@ class BSTNode {
      * @param {number} data The integer to store in the node.
      */
     constructor(data) {
-      this.data = data;
-      /**
-       * These properties are how this node is connected to other nodes to form
-       * the tree. Similar to .next in a SinglyLinkedList except a BST node can
-       * be connected to two other nodes. To start, new nodes will not be
-       * connected to any other nodes, these properties will be set after
-       * the new node is instantiated.
-       *
-       * @type {BSTNode|null}
-       */
-      this.left = null;
-      /** @type {BSTNode|null} */
-      this.right = null;
+        this.data = data;
+        /**
+         * These properties are how this node is connected to other nodes to form
+         * the tree. Similar to .next in a SinglyLinkedList except a BST node can
+         * be connected to two other nodes. To start, new nodes will not be
+         * connected to any other nodes, these properties will be set after
+         * the new node is instantiated.
+         *
+         * @type {BSTNode|null}
+         */
+        this.left = null;
+        /** @type {BSTNode|null} */
+        this.right = null;
     }
-  }
-  
-  /**
-   * Represents an ordered tree of nodes where the data of left nodes are <= to
-   * their parent and the data of nodes to the right are > their parent's data.
-   */
-  class BinarySearchTree {
+}
+
+/**
+ * Represents an ordered tree of nodes where the data of left nodes are <= to
+ * their parent and the data of nodes to the right are > their parent's data.
+ */
+class BinarySearchTree {
     constructor() {
-      /**
-       * Just like the head of a linked list, this is the start of our tree which
-       * branches downward from here.
-       *
-       * @type {BSTNode|null}
-       */
-      this.root = null;
+        /**
+         * Just like the head of a linked list, this is the start of our tree which
+         * branches downward from here.
+         *
+         * @type {BSTNode|null}
+         */
+        this.root = null;
     }
-  
+
     /**
      * Determines if this tree is empty.
      * - Time: O(?).
      * - Space: O(?).
      * @returns {boolean} Indicates if this tree is empty.
      */
-    isEmpty() {}
-  
+    isEmpty() { }
+
     /**
      * Retrieves the smallest integer data from this tree.
      * - Time: O(?).
@@ -54,8 +54,8 @@ class BSTNode {
      *    the tree is being traversed.
      * @returns {number} The smallest integer from this tree.
      */
-    min(current = this.root) {}
-  
+    min(current = this.root) { }
+
     /**
      * Retrieves the smallest integer data from this tree.
      * - Time: O(?).
@@ -64,8 +64,8 @@ class BSTNode {
      *    the tree is being traversed.
      * @returns {number} The smallest integer from this tree.
      */
-    minRecursive(current = this.root) {}
-  
+    minRecursive(current = this.root) { }
+
     /**
      * Retrieves the largest integer data from this tree.
      * - Time: O(?).
@@ -74,8 +74,8 @@ class BSTNode {
      *    the tree is being traversed.
      * @returns {number} The largest integer from this tree.
      */
-    max(current = this.root) {}
-  
+    max(current = this.root) { }
+
     /**
      * Retrieves the largest integer data from this tree.
      * - Time: O(?).
@@ -84,68 +84,68 @@ class BSTNode {
      *    the tree is being traversed.
      * @returns {number} The largest integer from this tree.
      */
-    maxRecursive(current = this.root) {}
-  
+    maxRecursive(current = this.root) { }
+
     // Logs this tree horizontally with the root on the left.
     print(node = this.root, spaceCnt = 0, spaceIncr = 10) {
-      if (!node) {
-        return;
-      }
-  
-      spaceCnt += spaceIncr;
-      this.print(node.right, spaceCnt);
-  
-      console.log(
-        " ".repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) +
-          `${node.data}`
-      );
-  
-      this.print(node.left, spaceCnt);
+        if (!node) {
+            return;
+        }
+
+        spaceCnt += spaceIncr;
+        this.print(node.right, spaceCnt);
+
+        console.log(
+            " ".repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) +
+            `${node.data}`
+        );
+
+        this.print(node.left, spaceCnt);
     }
-  }
-  
-  const emptyTree = new BinarySearchTree();
-  const oneNodeTree = new BinarySearchTree();
-  oneNodeTree.root = new BSTNode(10);
-  
-  /* twoLevelTree
-          root
-          10
-        /   \
-      5     15
-  */
-  const twoLevelTree = new BinarySearchTree();
-  twoLevelTree.root = new BSTNode(10);
-  twoLevelTree.root.left = new BSTNode(5);
-  twoLevelTree.root.right = new BSTNode(15);
-  
-  /* threeLevelTree 
-          root
-          10
-        /   \
-      5     15
-    / \    / \
-  2   6  13  
-  */
-  const threeLevelTree = new BinarySearchTree();
-  threeLevelTree.root = new BSTNode(10);
-  threeLevelTree.root.left = new BSTNode(5);
-  threeLevelTree.root.left.left = new BSTNode(2);
-  threeLevelTree.root.left.right = new BSTNode(6);
-  threeLevelTree.root.right = new BSTNode(15);
-  threeLevelTree.root.right.left = new BSTNode(13);
-  
-  /* fullTree
-                      root
-                  <-- 25 -->
-                /            \
-              15             50
-            /    \         /    \
-          10     22      35     70
-        /   \   /  \    /  \   /  \
-      4    12  18  24  31  44 66  90
-  */
-  /***************** Uncomment after insert method is created. ****************/
+}
+
+const emptyTree = new BinarySearchTree();
+const oneNodeTree = new BinarySearchTree();
+oneNodeTree.root = new BSTNode(10);
+
+/* twoLevelTree
+        root
+        10
+      /   \
+    5     15
+*/
+const twoLevelTree = new BinarySearchTree();
+twoLevelTree.root = new BSTNode(10);
+twoLevelTree.root.left = new BSTNode(5);
+twoLevelTree.root.right = new BSTNode(15);
+
+/* threeLevelTree 
+        root
+        10
+      /   \
+    5     15
+  / \    / \
+2   6  13  
+*/
+const threeLevelTree = new BinarySearchTree();
+threeLevelTree.root = new BSTNode(10);
+threeLevelTree.root.left = new BSTNode(5);
+threeLevelTree.root.left.left = new BSTNode(2);
+threeLevelTree.root.left.right = new BSTNode(6);
+threeLevelTree.root.right = new BSTNode(15);
+threeLevelTree.root.right.left = new BSTNode(13);
+
+/* fullTree
+                    root
+                <-- 25 -->
+              /            \
+            15             50
+          /    \         /    \
+        10     22      35     70
+      /   \   /  \    /  \   /  \
+    4    12  18  24  31  44 66  90
+*/
+/***************** Uncomment after insert method is created. ****************/
   // const fullTree = new BinarySearchTree();
   // fullTree
   //   .insert(25)
