d39f3a6d22b1ff4c52462361f86107c09177548e
Tyler Thibault <tyler.thibault@protonmail.com>
2023-03-17 08:58:41 -0600

    UPDATE FOR FRIDAY

--- BST/index.js
+++ BST/index.js
@@ -282,13 +282,13 @@ class BinarySearchTree {
    * @returns {Array<number>} The vals in DFS Preorder once all nodes visited.
    */
   toArrInorder(node = this.root, vals = []) {
-      if (node) {
-        this.toArrInorder(node.left, vals)
-        vals.push(node.data)
-        this.toArrInorder(node.right, vals)
-      }
-      return vals
+    if (node) {
+      this.toArrInorder(node.left, vals)
+      vals.push(node.data)
+      this.toArrInorder(node.right, vals)
     }
+    return vals
+  }
 
   /**
    * DFS Postorder (Left, Right, CurrNode)
@@ -309,6 +309,49 @@ class BinarySearchTree {
   }
 
   // ************************ END THURSDAY ************************
+  // ************************ FRIDAY ************************
+
+  /**
+ * BFS order: horizontal rows top-down left-to-right.
+ * Converts this BST into an array following Breadth First Search order.
+ * Example on the fullTree var:
+ * [25, 15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90]
+ * @param {Node} current The current node during the traversal of this tree.
+ * @returns {Array<number>} The data of all nodes in BFS order.
+ */
+  toArrLevelorder(current = this.root) { }
+
+  /**
+   * Recursively counts the total number of nodes in this tree.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {Node} node The current node during the traversal of this tree.
+   * @returns {number} The total number of nodes.
+   */
+  size(node = this.root) { }
+
+  /**
+   * Calculates the height of the tree which is based on how many nodes from
+   * top to bottom (whichever side is taller).
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {Node} node The current node during traversal of this tree.
+   * @returns {number} The height of the tree.
+   */
+  height(node = this.root) { }
+
+  /**
+   * Determines if this tree is a full tree. A full tree is a tree where every
+   * node has both a left and a right except for the leaf nodes (last nodes)
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {Node} node The current node during traversal of this tree.
+   * @returns {boolean} Indicates if this tree is full.
+   */
+  isFull(node = this.root) { }
+
+
+  // ************************ END FRIDAY ************************
 
   // Logs this tree horizontally with the root on the left.
   print(node = this.root, spaceCnt = 0, spaceIncr = 10) {
