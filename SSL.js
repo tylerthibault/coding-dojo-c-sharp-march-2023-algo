971dea7e0a561030a4a5670cb8d2edf898330bea
Tyler Thibault <tyler.thibault@protonmail.com>
2023-03-22 09:03:11 -0600

    UPLOAD FOR WEDNESDAY

--- SSL.js
+++ SSL.js
@@ -407,6 +407,33 @@ splitOnVal(val) {
 }
 
 // ********************* END FRIDAY *********************
+// ********************* WEDNESDAY W3 *********************
+/**
+ * Reverses this list in-place without using any extra lists.
+ * - Time: (?).
+ * - Space: (?).
+ * @returns {SinglyLinkedList} This list.
+ */
+reverse() {}
+
+/**
+ * Determines whether the list has a loop in it which would result in
+ * infinitely traversing unless otherwise avoided. A loop is when a node's
+ * next points to a node that is behind it.
+ * - Time: (?).
+ * - Space: (?).
+ * @returns {boolean} Whether the list has a loop or not.
+ */
+hasLoop() {}
+
+/**
+ * Removes all the nodes that have a negative integer as their data.
+ * - Time: (?).
+ * - Space: (?).
+ * @returns {SinglyLinkedList} This list after the negatives are removed.
+ */
+removeNegatives() {}
+// ********************* END WEDNESDAY W3 *********************
 
 /**
  * Converts this list into an array containing the data of each node.
